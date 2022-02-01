aa4d6b0ce4467f0ac00936629ddeb26626ca8b99
BrendenSeifried <brendenseifried@gmail.com>
2022-01-31 18:02:17 -0800

    taking break trying to get util working

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 // import functions and grab DOM elements REORGINIZE 
 const supply = document.getElementById('supplies');
 const listFood = document.getElementById('list-food');
-const listMeal = document.getElementById('list-meal');
+//const listMeal = document.getElementById('list-meal');
 const nameIt = document.getElementById('name-it');
 
+const clear = document.getElementById('undo');
 const save = document.getElementById('confirm');
 
 
@@ -22,6 +23,14 @@ let meals = [];
 //     console.log(statsfood);
 // }
 
+function remFood() {
+    listFood.textContent = '';
+    for (let count of counters) {
+        const li = remFood(count);
+        listFood.appendChild(li);
+    }
+}
+
 supply.addEventListener('submit', (e) => {
     e.preventDefault();
     const addSup = new FormData(supply);
@@ -32,7 +41,7 @@ supply.addEventListener('submit', (e) => {
         amount: Number(addSup.get('amount')),
      
     };
-    console.log(userData);
+    //console.log(userData);
 });
 //counters.push(userData);
 //console.log();
@@ -44,9 +53,18 @@ nameIt.addEventListener('click', (e) => {
     const userData = {
         name: addName.get('name'),
     };
-    console.log(userData);
+    //console.log(userData);
 });
 
+
+clear.addEventListener('click', () => {
+  // Step 2 -- add code to allow users to remove the most recent stat
+    counters.pop();
+    remFood();
+    
+  // Hint -- how do you remove an element from an array?
+  // Hint -- how can we rerender the stats using a function above?
+});
 // set event listeners 
   // get user input
   // use user input to update state 
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,18 @@
-import './example.test.js';
+
+
+import { ingredient, amount } from './utils.js';
+const test = QUnit.test;
+
+test('return stuff', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'bren, 12';
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = remFood({ ingredient: 'bren', amount: 12 });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
