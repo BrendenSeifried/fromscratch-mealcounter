856a126a06d5f8cb7b6316f19e4e379db10ade5b
BrendenSeifried <brendenseifried@gmail.com>
2022-02-03 13:52:54 -0800

    added some css styling

--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,18 @@
 
 
-// import { ingredient, amount } from '../utils.js';
-// const test = QUnit.test;
+import { meal.ingredient, meal.amount } from '../utils.js';
+const test = QUnit.test;
 
-// test('return stuff', (expect) => {
-//     //Arrange
-//     // Set up your arguments and expectations
-//     const expected = 'bren, 12';
+test('return stuff', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'bren, 12';
 
-//     //Act
-//     // Call the function you're testing and set the result to a const
-//     const actual = remFood({ ingredient: 'bren', amount: 12 });
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = remFood({ ingredient: 'bren', amount: 12 });
 
-//     //Expect
-//     // Make assertions about what is expected versus the actual result
-//     expect.equal(actual.outerHTML, expected);
-// });
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
