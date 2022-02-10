a4ffdfb2c82d7198b96077761f307810b727139b
BrendenSeifried <brendenseifried@gmail.com>
2022-02-09 17:15:50 -0800

    added second test

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,18 +1,37 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+//import { example } from '../example.js';
+
+import { rendFood, rendName } from '../utils.js';
+
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('return stuff', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
+    const expected = '<li>cat, 12 cups</li>';
+
+    //Act
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = rendFood({ ingredient: 'cat,', amount: 12, type: 'cups' });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
+
+test('return second test', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = '<li><p>undefined :</p><ul><li>undefined undefined undefined</li><li>undefined undefined undefined</li><li>undefined undefined undefined</li><li>undefined undefined undefined</li></ul></li>';
+
+    //Act
+    // Call the function you're testing and set the result to a const
+    const actual = rendName({ ingredients: 'test' });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
+
